f045fdbaf0e27fa3e264ed8755f9759257c9d7e7
GeorgeChryso <lsgechrys@gmail.com>
2023-02-02 11:02:31 +0200

    smtp transactional sent

--- server/serve.js
+++ server/serve.js
@@ -9,43 +9,42 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-
-// app.get("/", (req, res) => {
-//   res.send("Hello, World!");
-// });
-
-
 app.post('/send_mail',cors(),async (req,res)=>{
   console.log('incoming request',req.body)
   let { text } = req.body
-  const transport = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
-     
-})
 
-await transport.sendMail({
-	from: process.env.MAIL_FROM,
-	to: "bakoy13979@ekcsoft.com",
-	subject: "test email",
-	html: `<div className="email" style="
-      border: 1px solid black;
-      padding: 20px;
-      font-family: sans-serif;
-      line-height: 2;
-      font-size: 20px; 
-      ">
-      <h2>Here is your email!</h2>
-      <p>${text}</p>
-  
-      <p>All the best, Darwin</p>
-       </div>
-  `
-})
+    const transport = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      secure: false,
+      port: process.env.MAIL_PORT,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+        
+    })
+
+    await transport.sendMail({
+      from: process.env.MAIL_FROM,
+      to: "cilyxura@decabg.eu",
+      subject: "test email",
+      html: `<div className="email" style="
+          border: 1px solid black;
+          padding: 20px;
+          font-family: sans-serif;
+          line-height: 2;
+          font-size: 20px; 
+          ">
+          <h2>Here is your email!</h2>
+          <p>${text}</p>
+      
+          <p>All the best, Darwin</p>
+          </div>
+      `
+    })
 
     res.send('success')
 
